b6ee8578a26e15861205844ee6eb0aff25c2868f
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-04-01 17:43:00 +0430

    Add basic caching

--- ProxyServer.py
+++ ProxyServer.py
@@ -7,6 +7,7 @@ import pprint
 from bs4 import BeautifulSoup
 import logging
 import gzip
+import datetime
 
 BUFSIZE = 8192
 TIMEOUT = 10
@@ -62,10 +63,12 @@ class ProxyServer:
             logging.info("Accepted a request from client!")
             logging.info("Connection to [%s] from [%s] %s", self.serverSocket.getsockname()[0],
                          clientAddress[0], clientAddress[1])
-            HandlerThread(clientSocket, clientAddress, name=str(clientAddress[1])).start()
+            Thread(target=self.handlerThread, args=(clientSocket, clientAddress)).start()
+            # HandlerThread(clientSocket, clientAddress, name=str(clientAddress[1])).start()
 
     @staticmethod
     def handleHTTPInjection(parsedResponse, config):
+        # TODO: increase content-length
         if 'text/html' in parsedResponse.getHeader('content-type'):
             if 'gzip' in parsedResponse.getHeader('content-encoding'):
                 body = gzip.decompress(parsedResponse.getBody()).decode(encoding='UTF-8')
@@ -83,7 +86,8 @@ class ProxyServer:
                               'display: flex;' \
                               'justify-content: center;' \
                               'align-items: center;'
-            soup.body.insert(0, navbar)
+            if navbar not in soup.body:
+                soup.body.insert(0, navbar)
             if 'gzip' in parsedResponse.getHeader('content-encoding'):
                 body = gzip.compress(soup.encode())
             else:
@@ -123,48 +127,56 @@ class ProxyServer:
         # TODO: handle errors
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(('mail.ut.ac.ir', 25))
-        print(s.recv(1024))
-        s.send(b'HELO Hossein\r\n')
-        print(s.recv(2048))
+        s.recv(1024)
+        s.send(b'HELO mail.ut.ac.ir\r\n')
+        s.recv(2048)
         s.send(b'MAIL FROM: <%b>\r\n' % SEND_ADDR)
-        print(s.recv(2048))
+        s.recv(2048)
         s.send(b'AUTH LOGIN\r\n')
-        print(s.recv(2048))
+        s.recv(2048)
         s.send(b'%b\r\n' % MAIL_USER)
-        print(s.recv(2048))
+        s.recv(2048)
         s.send(b'%b\r\n' % MAIL_PASS)
-        print(s.recv(2048))
+        s.recv(2048)
         s.send(b'RCPT TO: <%b>\r\n' % RCPT_ADDR)
-        print(s.recv(2048))
+        s.recv(2048)
         s.send(b'DATA\r\n')
-        print(s.recv(2048))
+        s.recv(2048)
         s.send(b'To: %b' % RCPT_NAME + b' <%b>\r\n' % RCPT_ADDR)
         s.send(b'From: %b' % SEND_NAME + b' <%b>\r\n' % SEND_ADDR)
         s.send(b'Subject: Unauthorized access detected\r\n')
         s.send(packet)
         s.send(b'\r\n')
         s.send(b'.\r\n')
-        print(s.recv(2048))
+        s.recv(2048)
         s.send(b'QUIT\r\n')
         pass
 
+    @staticmethod
+    def canCache(response):
+        if len(response.getHeaders()) == 0:
+            return False
+        if response.getResponseCode() != 200:
+            return False
+        if 'cache-control' in response.getHeaders():
+            value = response.getHeader('cache-control')
+            if "private" in value or "no-cache" in value:
+                return False
+        if 'pragma' in response.getHeaders():
+            value = response.getHeader('pragma')
+            if "private" in value or "no-cache" in value:
+                return False
+        return True
 
-class HandlerThread(Thread):
-
-    def __init__(self, clientSocket, clientAddress, name):
-        super(HandlerThread, self).__init__(name=name)
-        self.clientSocket = clientSocket
-        self.clientAddress = clientAddress
-
-    def run(self):
-        request = ProxyServer.recvData(self.clientSocket)
-        if self.clientAddress[0] not in [u['IP'] for u in ProxyServer.config['accounting']['users']]:
-            self.clientSocket.close()
-            logging.info("User with ip [%s] has no permission no use proxy.", self.clientAddress[0])
+    def handlerThread(self, clientSocket, clientAddress):
+        request = self.recvData(clientSocket)
+        if clientAddress[0] not in [u['IP'] for u in self.config['accounting']['users']]:
+            clientSocket.close()
+            logging.info("User with ip [%s] has no permission no use proxy.", clientAddress[0])
             # TODO: send response and show an error message
             return
         else:
-            user = next((u for u in ProxyServer.config['accounting']['users'] if u['IP'] == self.clientAddress[0]), None)
+            user = next((u for u in self.config['accounting']['users'] if u['IP'] == clientAddress[0]), None)
         if len(request) > 0:
             parsedRequest = Tools.parseHTTP(request, 'request')
             logging.info('Client sent request to proxy with headers:\n'
@@ -173,57 +185,104 @@ class HandlerThread(Thread):
                          + '\n----------------------------------------------------------------------\n')
 
             parsedRequest.setHTTPVersion('HTTP/1.0')
-            if ProxyServer.config['restriction']['enable']:
-                for target in ProxyServer.config['restriction']['targets']:
+            if self.config['restriction']['enable']:
+                for target in self.config['restriction']['targets']:
                     if target['URL'] in parsedRequest.getURL():
-                        self.clientSocket.close()
+                        clientSocket.close()
                         if target['notify'] == 'true':
-                            ProxyServer.alertAdministrator(parsedRequest.pack())
+                            self.alertAdministrator(parsedRequest.pack())
                         return
+            if self.config['privacy']['enable']:
+                parsedRequest.setHeader('user-agent', self.config['privacy']['userAgent'])
 
-            if ProxyServer.config['privacy']['enable']:
-                parsedRequest.setHeader('user-agent', ProxyServer.config['privacy']['userAgent'])
+            if 'no-cache' in parsedRequest.getHeader('pragma') or 'no-cache' in parsedRequest.getHeader('cache-control'):
+                print('doesn\'t want to use cache')
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((parsedRequest.getWebServerAddress(), parsedRequest.getPort()))
+                logging.info("Proxy opening connection to server %s [%s]... Connection opened.",
+                             parsedRequest.getWebServerAddress(),
+                             socket.gethostbyname(parsedRequest.getWebServerAddress()))
+                s.sendall(parsedRequest.pack())
+                logging.info('Proxy sent request to server with headers:\n'
+                             + '----------------------------------------------------------------------\n'
+                             + parsedRequest.getHeaders().rstrip()
+                             + '\n----------------------------------------------------------------------\n')
+                response = self.recvData(s)
+                s.close()
+            else:
+                print('wants to use cache')
+                url = parsedRequest.getURL()
+                if url in self.cache:
+                    print('url in cache', url)
+                    if self.cache[url]['packet'].getHeader('expires') != "":
+                        expTime = datetime.datetime.strptime(self.cache[url]['packet'].getHeader('expires'), '%a, %d %b %Y %H:%M:%S GMT')
+                        currTime = datetime.datetime.now()
+                        if expTime < currTime:
+                            response = self.cache[url]['packet'].pack()
+                        else:
+                            print("Using cache")
+                            response = self.cache[url]['packet'].pack()
+                    else:
+                        lastMod = datetime.datetime.strptime(self.cache[url]['packet'].getHeader('last-modified'),
+                                                             '%a, %d %b %Y %H:%M:%S GMT')
+                        currTime = datetime.datetime.now()
+                        if lastMod < currTime:
+                            response = self.cache[url]['packet'].pack()
+                        else:
+                            print("Using cache")
+                            response = self.cache[url]['packet'].pack()
+                else:
+                    print('url not in cache', url)
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    s.connect((parsedRequest.getWebServerAddress(), parsedRequest.getPort()))
+                    logging.info("Proxy opening connection to server %s [%s]... Connection opened.",
+                                 parsedRequest.getWebServerAddress(),
+                                 socket.gethostbyname(parsedRequest.getWebServerAddress()))
+                    s.sendall(parsedRequest.pack())
+                    logging.info('Proxy sent request to server with headers:\n'
+                                 + '----------------------------------------------------------------------\n'
+                                 + parsedRequest.getHeaders().rstrip()
+                                 + '\n----------------------------------------------------------------------\n')
+                    response = self.recvData(s)
+                    s.close()
 
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((parsedRequest.getWebServerAddress(), parsedRequest.getPort()))
-            logging.info("Proxy opening connection to server %s [%s]... Connection opened.",
-                         parsedRequest.getWebServerAddress(),
-                         socket.gethostbyname(parsedRequest.getWebServerAddress()))
-            s.sendall(parsedRequest.pack())
-            logging.info('Proxy sent request to server with headers:\n'
-                         + '----------------------------------------------------------------------\n'
-                         + parsedRequest.getHeaders().rstrip()
-                         + '\n----------------------------------------------------------------------\n')
-            response = ProxyServer.recvData(s)
             if len(response):
                 parsedResponse = Tools.parseHTTP(response, 'response')
-                if int(user['volume']) < int(parsedResponse.getHeader('content-length')):
+                if self.config['HTTPInjection']['enable']:
+                    # TODO: check if the index page is requested
+                    parsedResponse = self.handleHTTPInjection(parsedResponse, self.config)
+                if self.canCache(parsedResponse) and parsedRequest.getURL() not in self.cache:
+                    print('caching', parsedRequest.getURL())
+                    self.cache[parsedRequest.getURL()] = {}
+                    self.cache[parsedRequest.getURL()]['packet'] = parsedResponse
+                else:
+                    print('not caching', parsedRequest.getURL())
+                if parsedResponse.getHeader('content-length') != "":
+                    contentLength = int(parsedResponse.getHeader('content-length'))
+                else:
+                    contentLength = parsedResponse.getBodySize()
+                if int(user['volume']) < contentLength:
                     logging.info("User ran out of traffic.")
-                    s.close()
-                    self.clientSocket.close()
+                    clientSocket.close()
                 else:
                     # TODO: check response status before reducing traffic
-                    newTraffic = int(user['volume']) - int(parsedResponse.getHeader('content-length'))
-                    for u in ProxyServer.config['accounting']['users']:
-                        if u['IP'] == self.clientAddress[0]:
+                    newTraffic = int(user['volume']) - contentLength
+                    for u in self.config['accounting']['users']:
+                        if u['IP'] == clientAddress[0]:
                             u['volume'] = str(newTraffic)
                             break
-                    if ProxyServer.config['HTTPInjection']['enable']:
-                        # TODO: check if the index page is requested
-                        parsedResponse = ProxyServer.handleHTTPInjection(parsedResponse, ProxyServer.config)
 
                     logging.info('Server sent response to proxy with headers:\n'
                                  + '----------------------------------------------------------------------\n'
                                  + parsedResponse.getHeaders().rstrip()
                                  + '\n----------------------------------------------------------------------\n')
-                    self.clientSocket.send(parsedResponse.pack())
+                    clientSocket.send(parsedResponse.pack())
                     logging.info('Proxy sent response to client with headers:\n'
                                  + '----------------------------------------------------------------------\n'
                                  + parsedResponse.getHeaders().rstrip()
                                  + '\n----------------------------------------------------------------------\n')
 
-                    s.close()
-                    self.clientSocket.close()
+                    clientSocket.close()
 
 
 if __name__ == '__main__':
